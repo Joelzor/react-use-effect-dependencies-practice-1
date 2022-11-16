6203534d93ffb11326d973175744fc7c3ce94a48
Joel Watkins <eck087@yahoo.co.uk>
2022-11-16 11:27:12 +0000

    finished exercise

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import "./styles.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
-  console.log({ data });
+  // console.log({ data });
+  // console.log(dataType);
 
   // Write code here.
   //
+  useEffect(() => {
+    fetch(`http://localhost:4000/${dataType}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [dataType]);
 
   return (
     <div>
       <SelectTypeForm setDataType={setDataType} />
-      {data && <DataList dataType={dataType} data={data.results} />}
+      {data && <DataList dataType={dataType} data={data} />}
     </div>
   );
 }
--- src/components/SelectTypeForm.js
+++ src/components/SelectTypeForm.js
@@ -1,10 +1,14 @@
 function SelectTypeForm(props) {
+  const { setDataType } = props;
   // Write code here...
+  const handleChange = (e) => {
+    setDataType(e.target.value);
+  };
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select id="type" name="type" onClick={handleChange}>
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
